bbc52d72ef62ded47236c799d992c397bb46a71b
Sarang095 <sarangchafekar8788459986@gmail.com>
2023-12-08 19:19:51 +0530

    Create Meathod

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     let addr = format!("0.0.0.0:{}", port);
 
-    let app = Router::new().route("/", get(health));
+    let app = Router::new()
+        .route("/", get(health));
+        .route("/quotes", post(handlers::create_quote));
+
     axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
         .await
@@ -16,4 +19,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 async fn health() -> http::StatusCode { 
     http::StatusCode::OK
-}
+
